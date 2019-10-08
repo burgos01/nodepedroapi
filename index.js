40c9c139728bea3c2eaef0d4a91e222b63e6c831
burgos01 <55356527+burgos01@users.noreply.github.com>
2019-10-08 14:44:52 -0300

    commited

---
+++ index.js
@@ -0,0 +1,42 @@
+const request = require('request');
+
+const API_URL = 'https://maps.googleapis.com/maps/api/geocode/json';
+const API_KEY = 'AIzaSyAqwwHBhv-WY2fXKCcnHxCzJZXp7F5GpEQ';
+
+const parseJson = (string) => {
+  try{
+    return JSON.parse(string);
+  }catch(e) {
+    return null;
+  }
+}
+
+const doRequest = (url) =>{
+  const promisseCallback = (resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) reject(error);
+      const data = parseJson(body);
+      resolve(data);
+    });
+  };
+  return new Promise(promisseCallback);
+}
+
+const getApiUrl = (address) => {
+	return `${API_URL}?key=${API_KEY}&address=${encodeURI(address)}`;
+}
+
+const address = 'Alto de Coutos - Salvador, BA';
+
+(async () => {
+  const apiUrl = getApiUrl(address);
+	const data = await doRequest(apiUrl);
+  
+  if (!data || data.error_message) {
+    const message = (data && data.error_message) ? data.error_message : 'Erro!!!';
+    console.log(message);
+    return;
+  }
+  
+  console.log(data.results[0].geometry.location);
+})();
